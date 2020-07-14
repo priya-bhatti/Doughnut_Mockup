c2b5c63dd530f29438c7bc547d5942adf14d2326
priya-bhatti <bhattip96@gmail.com>
2020-07-13 17:26:22 -0700

    image path in js test

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
+//const video = intro.querySelector('video');
 const branding = intro.querySelector('.branding');
 
 const menu = document.querySelector('.menu');
@@ -26,36 +26,36 @@ const end = menu.querySelector('h1');
 
 //SCROLL MAGIC IMG SEQ
 const images = [
-    "../images/0001.png",
-    "../images/0002.png",
-    "../images/0003.png",
-    "../images/0004.png",
-    "../images/0005.png",
-    "../images/0006.png",
-    "../images/0007.png",
-    "../images/0008.png",
-    "../images/0009.png",
-    "../images/0010.png",
-    "../images/0011.png",
-    "../images/0012.png",
-    "../images/0013.png",
-    "../images/0014.png",
-    "../images/0015.png",
-    "../images/0016.png",
-    "../images/0017.png",
-    "../images/0018.png",
-    "../images/0019.png",
-    "../images/0020.png",
-    "../images/0021.png",
-    "../images/0022.png",
-    "../images/0023.png",
-    "../images/0024.png",
-    "../images/0025.png",
-    "../images/0026.png",
-    "../images/0027.png",
-    "../images/0028.png",
-    "../images/0029.png",
-    "../images/0030.png",
+    "images/0001.png",
+    "images/0002.png",
+    "images/0003.png",
+    "images/0004.png",
+    "images/0005.png",
+    "images/0006.png",
+    "images/0007.png",
+    "images/0008.png",
+    "images/0009.png",
+    "images/0010.png",
+    "images/0011.png",
+    "images/0012.png",
+    "images/0013.png",
+    "images/0014.png",
+    "images/0015.png",
+    "images/0016.png",
+    "images/0017.png",
+    "images/0018.png",
+    "images/0019.png",
+    "images/0020.png",
+    "images/0021.png",
+    "images/0022.png",
+    "images/0023.png",
+    "images/0024.png",
+    "images/0025.png",
+    "images/0026.png",
+    "images/0027.png",
+    "images/0028.png",
+    "images/0029.png",
+    "images/0030.png",
 ];
 
 const obj = { curImg: 0 };
